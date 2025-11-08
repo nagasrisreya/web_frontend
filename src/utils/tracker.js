41d97d912114e9557fc57bccfbe9f7c07320d635
nagasrisreya <punnavajhalanagasrisreya@gmail.com>
2025-11-08 18:04:23 +0530

    check 1

--- src/utils/tracker.js
+++ src/utils/tracker.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL || "/api";
+const API_BASE = import.meta.env.VITE_API_URL || "https://web-backend-9vl0.onrender.com/api";
 
 /**
  * Send an event to backend
